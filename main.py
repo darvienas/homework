6489be7065abaec07d37ea20b571a3ddd66a029a
darvienas <45347399+darvienas@users.noreply.github.com>
2018-11-26 07:59:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
-'''Zip file attached contains newest Norway geospatial data for postal codes and their areas.
-
-Please draw Norway map separating each and every postal code area(could be a bit clustered in more
-densely populated areas) and provide the number of unique postal codes.
-
-When finished please create a repository in Github and upload the solution there.
-
-Result: map picture in easily readable format(.jpeg,.png etc.), number of unique numbers, link to Github repository.
-'''
-
 import matplotlib.pyplot as plt
 import geopandas as gpd
 
-
-# file_link = '/home/justis/PycharmProjects/homework/Basisdata_0000_Norge_25833_Postnummeromrader_SOSI_PostnummeromrЖde_FLATE.shp'
-
-file = gpd.read_file('Basisdata_0000_Norge_25833_Postnummeromrader_SOSI_PostnummeromrЖde_FLATE.shp')
+file_path = '~/Basisdata_0000_Norge_25833_Postnummeromrader_SOSI_PostnummeromrЖde_FLATE.shp'
+file = gpd.read_file(file_path)
 unique_codes = file['POSTNUMMER'].nunique()
 print('Unique values: ', unique_codes)
 
@@ -22,7 +10,8 @@ file.plot(figsize=(10, 10), edgecolor='black', linewidth=0.1, column='POSTNUMMER
 plt.title('Postal code areas in Norway')
 plt.xticks(ticks=[0, 1E6])
 plt.show()
-    # plt.savefig('map_N.jpeg')
+
+# plt.savefig('map_N.jpeg')
 
 
 
