9af2d474bd11c469a876041038200777000c5c3b
darvienas <45347399+darvienas@users.noreply.github.com>
2018-11-26 08:03:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import geopandas as gpd
 file_path = '~/Basisdata_0000_Norge_25833_Postnummeromrader_SOSI_PostnummeromrЖde_FLATE.shp'
 file = gpd.read_file(file_path)
 unique_codes = file['POSTNUMMER'].nunique()
-print('Unique values: ', unique_codes)
+print('Number of unique postal codes: ', unique_codes)
 
 file.plot(figsize=(10, 10), edgecolor='black', linewidth=0.1, column='POSTNUMMER')
 plt.title('Postal code areas in Norway')
